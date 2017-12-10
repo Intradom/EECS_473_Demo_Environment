d489d0f86a18ed4bdd4efb3f0f6ffdc5e2fb8eeb
Bo <bozhang10@yahoo.com>
2017-12-10 14:20:06 -0500

    Added parameter testing code

--- EMG/EMG_classification.py
+++ EMG/EMG_classification.py
@@ -42,6 +42,10 @@ NEGATIVE = 0
 SAMPLE_SIZE = 1000
 NUM_CLUSTERS = 10
 TRAINING_PORTION = 0.7
+REDUCED_BINS_SIZE = 200
+TEST_LOOP_TIMES = 20
+
+BIN_TESTS = [1, 5, 10, 20, 25, 50, 100, 200, 250, 500]
 
 fileOfBlinks = open("training.csv")
 row = []
@@ -73,7 +77,6 @@ class EMG_Classifier:
                 int_array = [int(numeric_string) for numeric_string in values]
                 training_samples[ch].extend(int_array)
             
-            """
             channels = ['ch0', 'ch1', 'ch2', 'ch3']
             for ch in channels:
                 # Demean
@@ -84,7 +87,6 @@ class EMG_Classifier:
                 training_samples[ch] = filtered_samples
                 #pyplot.plot(filtered_samples)
                 #pyplot.show()
-            """
             master_file.append(training_samples)
     
         #step 2
@@ -93,89 +95,97 @@ class EMG_Classifier:
             [file, channel, startTime] = line.split(',')
             csvFile.append([int(file), 'ch' + channel, int(1000*float(startTime))])
         
-        segmented_data = []
-        segmented_labels = []
+        for bin_size in BIN_TESTS:
+            segmented_data = []
+            segmented_labels = []
 
-        prev_s = -1
-        prev_c = -1
-        prev_seg_end = 0
-        for line in range(len(csvFile)):
-            #print(line)
-            # New section
-            s = csvFile[line][0] - 1 # Scenarios start are 1, but the arrays are 0-indexed
-            c = csvFile[line][1]
-            sp = csvFile[line][2] # starting point
-            print("Debug: " + str(s + 1) + ", " + str(c) + ", " + str(sp))
-            #pyplot.plot(master_file[s][c])
-            #pyplot.show()
-            if (s != prev_s or c != prev_c):
-                prev_seg_end = 0
-                # Take all prev neg examples if available
-                if (prev_s > 0 and prev_c > 0):
-                    negative_loops = int((sp - prev_seg_end) / SAMPLE_SIZE)
-                    for chunk in range(negative_loops):
-                        print("Negative prev")
-                        seg_start = prev_seg_end
-                        prev_seg_end = seg_start + SAMPLE_SIZE
-                        #print(seg_start)
-                        #print(prev_seg_end)
-                        target_segment = master_file[prev_s][prev_c][seg_start:prev_seg_end]
-                        #print(len(target_segment))
-                        segmented_data.append(self.segment_processing(target_segment, False))
-                        segmented_labels.append(NEGATIVE)
-                    
-            if (prev_seg_end > sp):
-                # Error in training file, abort
-                print("Error: " + str(s + 1) + ", " + str(c) + ", " + str(sp))
-                exit()
+            prev_s = -1
+            prev_c = -1
+            prev_seg_end = 0
+            for line in range(len(csvFile)):
+                #print(line)
+                # New section
+                s = csvFile[line][0] - 1 # Scenarios start are 1, but the arrays are 0-indexed
+                c = csvFile[line][1]
+                sp = csvFile[line][2] # starting point
+                #print("Debug: " + str(s + 1) + ", " + str(c) + ", " + str(sp))
+                #pyplot.plot(master_file[s][c])
+                #pyplot.show()
+                if (s != prev_s or c != prev_c):
+                    prev_seg_end = 0
+                    # Take all prev neg examples if available
+                    if (prev_s > 0 and prev_c > 0):
+                        negative_loops = int((sp - prev_seg_end) / SAMPLE_SIZE)
+                        for chunk in range(negative_loops):
+                            #print("Negative prev")
+                            seg_start = prev_seg_end
+                            prev_seg_end = seg_start + SAMPLE_SIZE
+                            #print(seg_start)
+                            #print(prev_seg_end)
+                            target_segment = master_file[prev_s][prev_c][seg_start:prev_seg_end]
+                            #print(len(target_segment))
+                            segmented_data.append(self.segment_processing(target_segment, False, bin_size))
+                            segmented_labels.append(NEGATIVE)
+                        
+                if (prev_seg_end > sp):
+                    # Error in training file, abort
+                    print("Error: " + str(s + 1) + ", " + str(c) + ", " + str(sp))
+                    exit()
 
-            # Get all negative samples before starting point
-            negative_loops = int((sp - prev_seg_end) / SAMPLE_SIZE)
-            print(negative_loops)
-            for chunk in range(negative_loops):
-                print("Negative")
-                seg_start = prev_seg_end
-                prev_seg_end = seg_start + SAMPLE_SIZE
-                print(seg_start)
-                print(prev_seg_end)
-                target_segment = master_file[s][c][seg_start:prev_seg_end]
-                #print(len(target_segment))
-                segmented_data.append(self.segment_processing(target_segment, False))
-                segmented_labels.append(NEGATIVE)
+                # Get all negative samples before starting point
+                negative_loops = int((sp - prev_seg_end) / SAMPLE_SIZE)
+                #print(negative_loops)
+                for chunk in range(negative_loops):
+                    #print("Negative")
+                    seg_start = prev_seg_end
+                    prev_seg_end = seg_start + SAMPLE_SIZE
+                    #print(seg_start)
+                    #print(prev_seg_end)
+                    target_segment = master_file[s][c][seg_start:prev_seg_end]
+                    #print(len(target_segment))
+                    segmented_data.append(self.segment_processing(target_segment, False, bin_size))
+                    segmented_labels.append(NEGATIVE)
 
-            # Get 1 positive sample if possible
-            if (sp + SAMPLE_SIZE < len(master_file[s][c])):
-                print("Positive")
-                prev_seg_end = sp + SAMPLE_SIZE
-                target_segment = master_file[s][c][sp:prev_seg_end]
-                segmented_data.append(self.segment_processing(target_segment, True))
-                segmented_labels.append(POSITIVE)
+                # Get 1 positive sample if possible
+                if (sp + SAMPLE_SIZE < len(master_file[s][c])):
+                    #print("Positive")
+                    prev_seg_end = sp + SAMPLE_SIZE
+                    target_segment = master_file[s][c][sp:prev_seg_end]
+                    segmented_data.append(self.segment_processing(target_segment, True, bin_size))
+                    segmented_labels.append(POSITIVE)
 
-        #print (segmented_labels)
-        
-        print("Kmeans: ")
-        kmeans = KMeans(n_clusters=NUM_CLUSTERS).fit(segmented_data)
-        #print(kmeans.labels_)
-        #print(segmented_labels)
-        # Find purity
-        for i in range(NUM_CLUSTERS):
-            #print(np.where(kmeans.labels_ == i)[0])
-            grouped_labels = np.array(segmented_labels)[np.where(kmeans.labels_ == i)[0]]
-            counts = np.bincount(grouped_labels)
-            print("Group " + str(i) + " marked as " + str(np.argmax(counts)) + ", size: " + str(grouped_labels.size) + ", purity: " + str(counts[np.argmax(counts)] / float(grouped_labels.size)))
-        
-        # RBF Kernal
-        guass_rbf = GaussianProcessClassifier(1.0 * RBF(1.0))
-        random_indicies = np.arange(len(segmented_data))
-        np.random.shuffle(random_indicies)
-        training_indicies = random_indicies[:int(random_indicies.size * TRAINING_PORTION)]
-        testing_indicies = random_indicies[int(random_indicies.size * TRAINING_PORTION):]
-        guass_rbf.fit(np.array(segmented_data)[training_indicies], np.array(segmented_labels)[training_indicies])
-        predicted_labels = guass_rbf.predict(np.array(segmented_data)[testing_indicies])
-        print("RBF accuracy: " + str(accuracy_score(np.array(segmented_labels)[testing_indicies], predicted_labels)))
-        tn, fp, fn, tp = confusion_matrix(np.array(segmented_labels)[testing_indicies], predicted_labels).ravel()
-        print("RBF TN/FP/FN/TP: " + str(tn) + "/" + str(fp) + "/" + str(fn) + "/" + str(tp))
-        
+            #print (segmented_labels)
+            
+            #print("Kmeans: ")
+            kmeans = KMeans(n_clusters=NUM_CLUSTERS).fit(segmented_data)
+            #print(kmeans.labels_)
+            #print(segmented_labels)
+            # Find purity
+            for i in range(NUM_CLUSTERS):
+                #print(np.where(kmeans.labels_ == i)[0])
+                grouped_labels = np.array(segmented_labels)[np.where(kmeans.labels_ == i)[0]]
+                counts = np.bincount(grouped_labels)
+                #print("Group " + str(i) + " marked as " + str(np.argmax(counts)) + ", size: " + str(grouped_labels.size) + ", purity: " + str(counts[np.argmax(counts)] / float(grouped_labels.size)))
+            
+            # RBF Kernal
+            guass_rbf = GaussianProcessClassifier(1.0 * RBF(1.0))
+            print("RBF: ")
+            avg = 0
+            for i in range(TEST_LOOP_TIMES):
+                random_indicies = np.arange(len(segmented_data))
+                np.random.shuffle(random_indicies)
+                training_indicies = random_indicies[:int(random_indicies.size * TRAINING_PORTION)]
+                testing_indicies = random_indicies[int(random_indicies.size * TRAINING_PORTION):]
+                guass_rbf.fit(np.array(segmented_data)[training_indicies], np.array(segmented_labels)[training_indicies])
+                predicted_labels = guass_rbf.predict(np.array(segmented_data)[testing_indicies])
+                #print("RBF accuracy: " + str(accuracy_score(np.array(segmented_labels)[testing_indicies], predicted_labels)))
+                tn, fp, fn, tp = confusion_matrix(np.array(segmented_labels)[testing_indicies], predicted_labels).ravel()
+                #print("RBF TN/FP/FN/TP: " + str(tn) + "/" + str(fp) + "/" + str(fn) + "/" + str(tp))
+                #print(tp/(fn + tp))
+                avg += tp/(fn + tp)
+            avg /= TEST_LOOP_TIMES
+            print(str(bin_size) + " average: " + str(avg))
+         
         ch_name = "ch0"
         detected_values = []
         ch_filter = moving_avg_filter_t(250, 250, -0.25, 500)
@@ -221,8 +231,9 @@ class EMG_Classifier:
         while True:
             sleep(1)
  
-    def segment_processing(self, segment, pos):
+    def segment_processing(self, segment, pos, bin_size):
         segment -= np.mean(segment)
+        
         """
         pyplot.plot(segment)
         if (pos):
@@ -231,14 +242,24 @@ class EMG_Classifier:
             pyplot.title("Negative")
         pyplot.show()
         """
-        # FFT into bins of lower frequencies
+        
+        """ FFT into bins of lower frequencies
         #fft_powers = abs(np.fft.fft(segment))
         #processed = fft_powers[:int(fft_powers.size / 4)]
+        """
         
-        # Scaled time domain
+        #""" Scaled time domain
         seg_max = segment[np.argmax(segment)]
         seg_min = segment[np.argmin(segment)]
-        processed = segment / float(seg_max - seg_min)
+        pre_reduction = segment / float(seg_max - seg_min)
+        # Break down bins
+        processed = []
+        average_amount = int(SAMPLE_SIZE / bin_size)
+        #print("Before: " + str(len(pre_reduction)))
+        for i in range(bin_size):
+            processed.append(np.average(pre_reduction[i*average_amount:(i+1)*average_amount]))
+        #print("After: " + str(len(processed)))
+        #"""
         
         #pyplot.plot(processed)
         #pyplot.show()
